53a4a442615047158a315a3356e66739d9a64608
Jack <hausmeistar@protonmail.com>
2023-06-22 10:35:38 +0200

    Versuch Nr.2

--- app/src/main/java/com/example/diy_project_interface_app/Modules/Module.java
+++ app/src/main/java/com/example/diy_project_interface_app/Modules/Module.java
@@ -1,2 +1,102 @@
-package com.example.diy_project_interface_app.Modules;public class Module {
+package com.example.diy_project_interface_app.Modules;
+
+import android.view.View;
+
+/***
+ * Author: Jonas Sbiegay
+ * Studiengang: SMSB
+ * Created: 18.06.2023
+ * ModuleClass is the base for the specific modules in the GUI
+ */
+
+public class Module {
+    private int moduleTypeId;
+    private String name;
+    private int width, height;
+    private int xPosition, yPosition;
+    private View relatedView;
+    private int creationId;
+    private String title;
+    private boolean isEnabled;
+    private String description;
+    private boolean informationChanged;
+    private String information;
+
+    public Module(int moduleTypeId, String name, int width, int height, View relatedView) {
+        this.moduleTypeId = moduleTypeId;
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.relatedView = relatedView;
+        this.isEnabled = false;
+        this.description = "";
+        this.informationChanged = false;
+        this.information = "";
+    }
+
+    public void updateInformation(String receivedString) {
+
+    }
+
+    public String getInformation() {
+        if (informationChanged) {
+            informationChanged = false;
+            return information;
+        }
+        return "";
+    }
+
+
+    // relevante Getter und Setter
+    public String getName() {
+        return name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    public void setXPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getYPosition() {
+        return yPosition;
+    }
+
+    public void setYPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public int getCreationId() {
+        return creationId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- app/src/main/java/com/example/diy_project_interface_app/Modules/ModuleSlider.java
+++ app/src/main/java/com/example/diy_project_interface_app/Modules/ModuleSlider.java
@@ -1,2 +1,23 @@
-package com.example.diy_project_interface_app.Modules;public class ModuleSlider {
+package com.example.diy_project_interface_app.Modules;
+
+import android.view.View;
+
+public class ModuleSlider extends Module {
+
+    private int moduleTypeId;
+    private String name;
+    private int width, height;
+    private int xPosition, yPosition;
+    private View relatedView;
+    private int creationId;
+    private String title;
+    private boolean isEnabled;
+    private String description;
+    private boolean informationChanged;
+    private String information;
+
+    public ModuleSlider(int moduleTypeId, String name, int width, int height, View relatedView) {
+
+    }
+
 }
