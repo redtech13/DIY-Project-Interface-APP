7dfec9c9075852fe731ab5738f28879d7b4cec97
Jack <hausmeistar@protonmail.com>
2023-06-22 23:10:57 +0200

    Erster Ansatz für Slider überlegt, ohne XML bis jetzt

--- app/src/main/java/com/example/diy_project_interface_app/Modules/ModuleSlider.java
+++ app/src/main/java/com/example/diy_project_interface_app/Modules/ModuleSlider.java
@@ -5,10 +5,34 @@ import android.view.View;
 public class ModuleSlider extends Module {
 
     protected float sliderValue;
+    protected SeekBar slider = findViewById(R.id.slider);
 
     public ModuleSlider(int moduleTypeId, String name, int width, int height, View relatedView, float sliderValue) {
         super(moduleTypeId, name, width, height, relatedView);
         this.sliderValue = sliderValue;
     }
+
+
+    //auf der Seekbar will ich unseren Slider aufbauen morgen
+    slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            // Hier wird der ausgewählte Wert aktualisiert
+            // progress enthält dann den aktuellen Wert des Sliders
+        }
+
+        // Wird aufgerufen, wenn Slider berührt wird
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        // Wird aufgerufen, wenn der Slider released wird
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    });
+
 
 }
