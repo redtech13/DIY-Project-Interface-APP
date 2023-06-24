8a489bfb294863ef818cd6cbd461190efd79ac34
Jack <hausmeistar@protonmail.com>
2023-06-24 10:51:01 +0200

    Verbesserung für Multiinstanz (keine statische R. ID)

--- app/src/main/java/com/example/diy_project_interface_app/Modules/ModuleSlider.java
+++ app/src/main/java/com/example/diy_project_interface_app/Modules/ModuleSlider.java
@@ -1,38 +1,44 @@
 package com.example.diy_project_interface_app.Modules;
 
 import android.view.View;
+import android.widget.SeekBar;
 
 public class ModuleSlider extends Module {
 
     protected float sliderValue;
-    protected SeekBar slider = findViewById(R.id.slider);
 
     public ModuleSlider(int moduleTypeId, String name, int width, int height, View relatedView, float sliderValue) {
         super(moduleTypeId, name, width, height, relatedView);
         this.sliderValue = sliderValue;
+
+        initSlider();
     }
 
+    private void initSlider(){
+        SeekBar slider = (SeekBar) relatedView;
 
-    //auf der Seekbar will ich unseren Slider aufbauen morgen
-    slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            // Hier wird der ausgewählte Wert aktualisiert
-            // progress enthält dann den aktuellen Wert des Sliders
-        }
+        //auf der Seekbar will ich unseren Slider aufbauen morgen
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Hier wird der ausgewählte Wert aktualisiert
+                // progress enthält dann den aktuellen Wert des Sliders
+            }
 
-        // Wird aufgerufen, wenn Slider berührt wird
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
+            // Wird aufgerufen, wenn Slider berührt wird
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-        }
+            }
 
-        // Wird aufgerufen, wenn der Slider released wird
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
+            // Wird aufgerufen, wenn der Slider released wird
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
-        }
-    });
+            }
+        });
+    }
+
 
 
 }
