33de36e0d1a9c72b9e1cbc0634f02c22f5ca1415
Jack <hausmeistar@protonmail.com>
2023-06-28 13:59:06 +0200

    Main Anpassungen

--- app/src/main/java/com/example/diy_project_interface_app/MainActivity.java
+++ app/src/main/java/com/example/diy_project_interface_app/MainActivity.java
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
     private String buildRequest(){
         //Todo: request build info from device
         //return "_-_´5;`´0;4;1`___"; //Testing
-        return "_-_´5;`´0;4;1`´0;1;3`´0;3;2`´0;5;1`´0;1;2`´0;1;3`´0;1;3`´0;2;2`´0;1;3`´0;2;2`´0;3;1`´0;2;1`___"; //Testing
+        return "_-_´5;`´0;4;1`´3;1;3`´0;3;2`´3;5;1`´0;1;2`´0;1;3`´0;1;3`´0;2;2`´0;1;3`´0;2;2`´0;3;1`´0;2;1`___"; //Testing
     }
 
     private void buildReset(){
